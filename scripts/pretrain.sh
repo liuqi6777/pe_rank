e199f04dc481210d0e871bb27a2f36bf0df0b180
liuqi6777 <qiliu6777@gmail.com>
2024-02-06 12:26:38 +0100

    update pretrain script

--- scripts/pretrain.sh
+++ scripts/pretrain.sh
@@ -3,20 +3,19 @@
 export WANDB_PROJECT="embedding+llm"
 
 python src/train.py \
-    --model_name_or_path JackFram/llama-68m \
+    --model_name_or_path lmsys/vicuna-7b-v1.5 \
     --data_path ./data/msmarco-100k-clean-train.jsonl \
     --encoder_name jinaai/jina-embeddings-v2-base-en \
     --projector_type mlp2x_gelu \
-    --tune_mlp_adapter True \
-    --bf16 True \
+    --tune_mlp_adapter \
+    --bf16 \
     --output_dir ./checkpoints/pretrain-test \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 32 \
-    --per_device_eval_batch_size 4 \
+    --per_device_train_batch_size 16 \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 24000 \
+    --save_steps 500 \
     --save_total_limit 1 \
     --learning_rate 1e-3 \
     --weight_decay 0. \
@@ -25,5 +24,6 @@ python src/train.py \
     --logging_steps 1 \
     --tf32 True \
     --model_max_length 2048 \
-    --gradient_checkpointing True \
-    --dataloader_num_workers 4
+    --gradient_checkpointing \
+    --dataloader_num_workers 4 \
+    --report_to "wandb"
