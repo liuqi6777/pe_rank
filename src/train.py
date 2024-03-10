9655cecf0800ffb79a2808323b5de52d358089c5
liuqi6777 <qiliu6777@gmail.com>
2024-03-10 04:57:20 -0400

    refactor: move  to trainer.py

--- src/train.py
+++ src/train.py
@@ -1,17 +1,16 @@
 import math
 import os
 import pathlib
-from typing import Dict, Optional, Sequence
 
 from peft import LoraConfig, get_peft_model
 import torch
 import transformers
-from transformers import Trainer
 
 from arguments import ModelArguments, DataArguments, TrainingArguments, LoraArguments
 from data import make_data_module
 from modeling.causal_lm import EmbedLlamaForCausalLM
 from modeling.rank_lm import EmbedLlamaForRankLM
+from trainer import Trainer
 from utils import *
 
 
@@ -21,33 +20,6 @@ local_rank = None
 def rank0_print(*args):
     if local_rank == 0:
         print(*args)
-
-
-class Trainer(Trainer):
-    def _save_checkpoint(self, model, trial, metrics=None):
-        if getattr(self.args, 'tune_mlp_adapter', False):
-            from transformers.trainer_utils import PREFIX_CHECKPOINT_DIR
-            checkpoint_folder = f"{PREFIX_CHECKPOINT_DIR}-{self.state.global_step}"
-
-            run_dir = self._get_output_dir(trial=trial)
-            output_dir = os.path.join(run_dir, checkpoint_folder)
-
-            # Only save Adapter
-            keys_to_match = ['projector']
-
-            weight_to_save = get_adapter_state_maybe_zero_3(self.model.named_parameters(), keys_to_match)
-
-            if self.args.local_rank == 0 or self.args.local_rank == -1:
-                self.model.config.save_pretrained(output_dir)
-                torch.save(weight_to_save, os.path.join(output_dir, f'projector.bin'))
-        else:
-            super()._save_checkpoint(model.get_model(), trial, metrics)
-
-    def _save(self, output_dir: Optional[str] = None, state_dict=None):
-        if getattr(self.args, 'tune_mlp_adapter', False):
-            pass
-        else:
-            super()._save(output_dir, state_dict)
 
 
 def train():
---
+++ src/trainer.py
@@ -0,0 +1,34 @@
+import os
+import torch
+from typing import Optional
+
+from transformers import Trainer as HFTrainer
+
+from utils import get_adapter_state_maybe_zero_3
+
+
+class Trainer(HFTrainer):
+    def _save_checkpoint(self, model, trial, metrics=None):
+        if getattr(self.args, 'tune_mlp_adapter', False):
+            from transformers.trainer_utils import PREFIX_CHECKPOINT_DIR
+            checkpoint_folder = f"{PREFIX_CHECKPOINT_DIR}-{self.state.global_step}"
+
+            run_dir = self._get_output_dir(trial=trial)
+            output_dir = os.path.join(run_dir, checkpoint_folder)
+
+            # Only save Adapter
+            keys_to_match = ['projector']
+
+            weight_to_save = get_adapter_state_maybe_zero_3(self.model.named_parameters(), keys_to_match)
+
+            if self.args.local_rank == 0 or self.args.local_rank == -1:
+                self.model.config.save_pretrained(output_dir)
+                torch.save(weight_to_save, os.path.join(output_dir, f'projector.bin'))
+        else:
+            super()._save_checkpoint(model.get_model(), trial, metrics)
+
+    def _save(self, output_dir: Optional[str] = None, state_dict=None):
+        if getattr(self.args, 'tune_mlp_adapter', False):
+            pass
+        else:
+            super()._save(output_dir, state_dict)
