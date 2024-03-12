1706e11e7c40921abb413c9da7fcd312ccde718e
liuqi6777 <qiliu6777@gmail.com>
2024-03-12 16:28:35 +0100

    update scripts

--- scripts/pretrain.sh
+++ scripts/pretrain.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
 
+set -e
+
 export WANDB_PROJECT="embedding+llm"
 
-deepspeed --include="localhost:1,2,3,4" src/train.py \
+deepspeed --include="localhost:0,1,2,3" src/train.py \
     --deepspeed scripts/zero2.json \
     --model_name_or_path lmsys/vicuna-7b-v1.5 \
-    --data_path ./data/msmarco-100k-clean-train.jsonl \
+    --data_path ./data/wiki_dpr_pretrain.jsonl \
     --encoder_name jinaai/jina-embeddings-v2-base-en \
     --projector_type mlp2x_gelu \
     --freeze_backbone \
     --tune_mlp_adapter \
     --bf16 \
-    --output_dir ./checkpoints/vicuna.jina.rankgpt100k.pretrain \
+    --output_dir ./checkpoints/vicuna.jina.wiki1m.pretrain \
     --num_train_epochs 1 \
     --per_device_train_batch_size 32 \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 500 \
+    --save_steps 1000 \
     --save_total_limit 1 \
     --learning_rate 1e-3 \
-    --weight_decay 0. \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
     --tf32 True \
-    --model_max_length 2048 \
+    --model_max_length 512 \
     --gradient_checkpointing \
     --dataloader_num_workers 4 \
     --report_to "wandb"
