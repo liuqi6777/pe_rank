aa222b30228490fb4776eb2bce0d306fee0aeb3d
liuqi6777 <qiliu6777@gmail.com>
2024-03-14 08:52:24 -0400

    feat: lora builder

--- src/modeling/builder.py
+++ src/modeling/builder.py
@@ -1,12 +1,18 @@
 import os
-
-from transformers import AutoTokenizer, AutoModelForCausalLM, AutoConfig, BitsAndBytesConfig
 import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, AutoConfig, BitsAndBytesConfig
+from huggingface_hub import hf_hub_download
+from peft import PeftModel, PeftConfig
 
 from constants import PLACEHOLDER
 from modeling.encoder import Encoder, build_projector
 from modeling.causal_lm import EmbedLlamaForCausalLM
 from modeling.rank_lm import EmbedLlamaForRankLM
+
+
+def load_from_hf(repo_id, filename, subfolder=None):
+    cache_file = hf_hub_download(repo_id=repo_id, filename=filename, subfolder=subfolder)
+    return torch.load(cache_file, map_location='cpu')
 
 
 def load_pretrained_model(
@@ -42,50 +48,45 @@ def load_pretrained_model(
         kwargs['attn_implementation'] = 'flash_attention_2'
 
     if 'embed' in model_name.lower():
-        if 'lora' in model_name.lower() and model_base is not None:
-            lora_cfg_pretrained = AutoConfig.from_pretrained(model_path)
-            tokenizer = AutoTokenizer.from_pretrained(
-                model_base, use_fast=False)
-            model = EmbedLlamaForCausalLM.from_pretrained(model_base, low_cpu_mem_usage=True, config=lora_cfg_pretrained, **kwargs)
-            token_num, tokem_dim = model.lm_head.out_features, model.lm_head.in_features
-            if model.lm_head.weight.shape[0] != token_num:
-                model.lm_head.weight = torch.nn.Parameter(torch.empty(token_num, tokem_dim, device=model.device, dtype=model.dtype))
-                model.model.embed_tokens.weight = torch.nn.Parameter(torch.empty(token_num, tokem_dim, device=model.device, dtype=model.dtype))
+        if os.path.exists(os.path.join(model_path, 'adapter_config.json')):
+            # load lora model
+            lora_cfg_pretrained = PeftConfig.from_pretrained(model_path)
+            if not model_base:
+                model_base = lora_cfg_pretrained.base_model_name_or_path
+            tokenizer = AutoTokenizer.from_pretrained(model_base, use_fast=False)
+            cfg_pretrained = AutoConfig.from_pretrained(model_path)
+            cfg_pretrained.vocab_size = len(tokenizer)
+            model = EmbedLlamaForRankLM.from_pretrained(model_base, low_cpu_mem_usage=True, config=cfg_pretrained, **kwargs)
+            model.initialize_tokenizer(tokenizer)
+
+            model.get_model().encoder = Encoder(cfg_pretrained.encoder_name, cfg_pretrained)
+            model.get_model().projector = build_projector(cfg_pretrained)
+            model.get_encoder().to("cuda")   # FIXME
+            model.get_projector().to("cuda")
 
             if os.path.exists(os.path.join(model_path, 'non_lora_trainables.bin')):
                 non_lora_trainables = torch.load(os.path.join(model_path, 'non_lora_trainables.bin'), map_location='cpu')
             else:
-                # this is probably from HF Hub
-                from huggingface_hub import hf_hub_download
-
-                def load_from_hf(repo_id, filename, subfolder=None):
-                    cache_file = hf_hub_download(
-                        repo_id=repo_id,
-                        filename=filename,
-                        subfolder=subfolder)
-                    return torch.load(cache_file, map_location='cpu')
-                non_lora_trainables = load_from_hf(
-                    model_path, 'non_lora_trainables.bin')
+                non_lora_trainables = load_from_hf(model_path, 'non_lora_trainables.bin')
             non_lora_trainables = {(k[11:] if k.startswith(
                 'base_model.') else k): v for k, v in non_lora_trainables.items()}
             if any(k.startswith('model.model.') for k in non_lora_trainables):
-                non_lora_trainables = {(k[6:] if k.startswith(
-                    'model.') else k): v for k, v in non_lora_trainables.items()}
+                non_lora_trainables = {(k[6:] if k.startswith('model.') else k): v
+                                       for k, v in non_lora_trainables.items()}
             model.load_state_dict(non_lora_trainables, strict=False)
 
-            from peft import PeftModel
             print('Loading LoRA weights...')
             model = PeftModel.from_pretrained(model, model_path)
             print('Merging LoRA weights...')
             model = model.merge_and_unload()
             print('Model is loaded...')
+
         elif model_base is not None:
             # this may be projector only
             print('Loading model from base model...')
-            tokenizer = AutoTokenizer.from_pretrained(
-                model_base, use_fast=False)
+            tokenizer = AutoTokenizer.from_pretrained(model_base, use_fast=False)
             cfg_pretrained = AutoConfig.from_pretrained(model_path)
-            cfg_pretrained.vocab_size -= 2  # FIXME
+            cfg_pretrained.vocab_size = len(tokenizer)
             model = EmbedLlamaForRankLM.from_pretrained(model_base, low_cpu_mem_usage=True, config=cfg_pretrained, **kwargs)
             model.initialize_tokenizer(tokenizer)
 
@@ -121,11 +122,8 @@ def load_pretrained_model(
         # Load language model
         if model_base is not None:
             # PEFT model
-            from peft import PeftModel
-            tokenizer = AutoTokenizer.from_pretrained(
-                model_base, use_fast=False)
-            model = AutoModelForCausalLM.from_pretrained(
-                model_base, low_cpu_mem_usage=True, **kwargs)
+            tokenizer = AutoTokenizer.from_pretrained(model_base, use_fast=False)
+            model = AutoModelForCausalLM.from_pretrained(model_base, low_cpu_mem_usage=True, **kwargs)
             print(f"Loading LoRA weights from {model_path}")
             model = PeftModel.from_pretrained(model, model_path)
             print(f"Merging weights")
