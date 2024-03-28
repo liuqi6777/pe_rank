d9ba6d376a21bd13f10d57df0ed108a8468bb45d
liuqi6777 <qiliu6777@gmail.com>
2024-03-28 03:56:12 -0400

    feat: sliding window ranking

--- src/eval_rank.py
+++ src/eval_rank.py
@@ -4,104 +4,23 @@ import os
 import torch
 from tqdm import tqdm
 from transformers import AutoTokenizer
-from fastchat.model.model_adapter import get_conversation_template
 
 from modeling.builder import load_pretrained_model
+from modeling.rank_lm.reranker import RerankLLM
 
 
-PLACEHOLDER = "<PLACEHOLDER>"
-INSTRUCTION = """Providing with {n} passages, each is enclosed in the identifier []. Rank the passages based on their relevance to the search query: {query}. 
-{inputs}
-Search Query: {query}. Rank the {n} passages above based on their relevance to the search query in descending order.
-"""
-
-
-def process_data(data):
-    new_data = []
-    for d in data:
-        retrieved_passages = d["hits"]
-        new_data.append({
-            "conversations": [
-                {
-                    "from": "human",
-                    "value": INSTRUCTION.format(
-                        n=len(retrieved_passages),
-                        query=d["query"],
-                        inputs="\n".join(["[<PLACEHOLDER>]"]
-                                         * len(retrieved_passages))
-                    )
-                },
-            ],
-            "extra_texts": [p["content"] for p in retrieved_passages]
-        })
-
-    # process data
-    conv = get_conversation_template("vicuna")
-    roles = {"human": conv.roles[0], "gpt": conv.roles[1]}
-    conversations = []
-    extra_texts = []
-    for i, source in enumerate(new_data):
-        conversation = source["conversations"]
-        if roles[conversation[0]["from"]] != conv.roles[0]:
-            # Skip the first one if it is not from human
-            conversation = conversation[1:]
-        conv.messages = []
-        for j, sentence in enumerate(conversation):
-            role = roles[sentence["from"]]
-            assert role == conv.roles[j % 2], f"{i}"
-            conv.append_message(role, sentence["value"])
-        conv.append_message(conv.roles[1], None)
-        conversations.append(conv.get_prompt())
-        extra_texts.append(source["extra_texts"])
-    return conversations, extra_texts
-
-
-def prepare_input(conversations, extra_texts, tokenizer, encoder_tokenizer):
-    if tokenizer.pad_token_id is None:
-        tokenizer.pad_token = tokenizer.eos_token
-    input_ids = tokenizer(
-        conversations,
-        return_tensors="pt",
-        padding="longest",
-        max_length=tokenizer.model_max_length,
-        truncation=True,
-    ).input_ids
-    extra_text_input_ids = []
-    for texts in extra_texts:
-        extra_text_input_ids.append(
-            encoder_tokenizer(
-                texts,
-                return_tensors="pt",
-                padding="max_length",
-                max_length=200,
-                truncation=True,
-            ).input_ids
-        )
-    extra_text_input_ids = torch.nn.utils.rnn.pad_sequence(
-        extra_text_input_ids,
-        batch_first=True,
-        padding_value=encoder_tokenizer.pad_token_id
-    )
-    return input_ids, extra_text_input_ids
-
-
-def write_results(data, ranks, output_file):
-    results = []
-    for i, rank in enumerate(ranks):
-        result = []
-        hits = data[i]["hits"]
-        for j, r in enumerate(rank):
-            result.append(f"{hits[0]['qid']} Q{i} {hits[r - 1]['docid']} {j + 1} {round(1 / (j + 1), 3)} rank")
-        results.append(result)
+def write_results(rerank_results, output_file):
     with open(output_file, "w") as f:
-        for result in results:
-            f.write("\n".join(result))
-            f.write("\n")
+        for i, result in enumerate(rerank_results):
+            hits = result["hits"]
+            for j, hit in enumerate(hits):
+                f.write(f"{hit['qid']} Q{i} {hit['docid']} {j + 1} {round(1 / (j + 1), 3)} rank")
+                f.write("\n")
 
 
 def eval_model(args):
 
-    tokenizer, model, context_len = load_pretrained_model(
+    tokenizer, model, _ = load_pretrained_model(
         model_path=args.model_path,
         model_base=args.model_base,
         model_name="embed_llama",
@@ -113,35 +32,45 @@ def eval_model(args):
 
     encoder_tokenizer = AutoTokenizer.from_pretrained(config.encoder_name)
 
+    reranker_llm = RerankLLM(
+        model=model,
+        tokenizer=tokenizer,
+        encoder_tokenizer=encoder_tokenizer,
+    )
+
     for dataset in args.datasets:
 
         reranker = (args.model_path or args.model_base).split("/")[-1]
-        if not args.alias:
-            output_file = f"results/rerank_results/{args.retriever}/eval_{dataset}_{reranker}_top{args.topk}.txt"
-        else:
-            output_file = f"results/rerank_results/{args.retriever}/eval_{dataset}_{reranker}_top{args.topk}_{args.alias}.txt"
+        output_file = os.path.join(
+            "results", "rerank_results", args.retriever,
+            f"eval_{dataset}_{reranker}_{args.rerank_mode}_top{args.topk}.txt"
+        )
         if os.path.exists(output_file) and not args.overwrite:
             print(f"{output_file} exists, skipping")
             continue
 
-        input_file = f"results/retrieval_results/{args.retriever}/{dataset}_top{args.topk}.jsonl"
+        input_file = os.path.join(
+            "results", "retrieval_results", args.retriever,
+            f"{dataset}_top{args.topk}.jsonl"
+        )
         with open(input_file, "r") as f:
             data = [json.loads(line) for line in f]
-        conversations, extra_texts = process_data(data)
-        input_ids, extra_text_input_ids = prepare_input(conversations, extra_texts, tokenizer, encoder_tokenizer)
 
-        ranks = []
-        with torch.inference_mode():
-            for i in tqdm(range(input_ids.shape[0])):
-                rank = model.rank(
-                    input_ids[[i]],
-                    extra_text_input_ids=extra_text_input_ids[[i]],
-                    extra_text_attention_mask=extra_text_input_ids.ne(encoder_tokenizer.pad_token_id)[[i]],
-                    use_cache=True,
-                )
-                ranks.append(rank)
+        if args.rerank_mode == "direct":
+            window_size, step = None, None
+        elif args.rerank_mode.startswith("sliding"):
+            _, window_size, step = args.rerank_mode.split("-")
+            window_size, step = int(window_size), int(step)
 
-        write_results(data, ranks, output_file)
+        rerank_results = []
+        for i in tqdm(range(len(data))):
+            rerank_result = reranker_llm.rerank(
+                retrieved_result=data[i],
+                window_size=window_size,
+                step=step,
+            )
+            rerank_results.append(rerank_result)
+        write_results(rerank_results, output_file)
 
 
 if __name__ == "__main__":
@@ -150,8 +79,8 @@ if __name__ == "__main__":
     parser.add_argument("--model-base", type=str, default=None)
     parser.add_argument("--datasets", nargs="+", default=["dl19"])
     parser.add_argument("--retriever", type=str, default="bm25")
-    parser.add_argument("--topk", type=int, default=20)
+    parser.add_argument("--topk", type=int, default=100)
+    parser.add_argument("--rerank-mode", type=str, default="direct")
     parser.add_argument("--overwrite", action="store_true")
-    parser.add_argument("--alias", type=str, default=None)
     args = parser.parse_args()
     eval_model(args)
---
+++ src/modeling/rank_lm/reranker.py
@@ -0,0 +1,135 @@
+import copy
+import transformers
+import torch
+from typing import Optional
+from fastchat.model.model_adapter import get_conversation_template
+
+from modeling.rank_lm.modeling_llama import EmbedLlamaForRankLM
+
+
+PLACEHOLDER = "<PLACEHOLDER>"
+INSTRUCTION = """Providing with {n} passages, each is enclosed in the identifier []. Rank the passages based on their relevance to the search query: {query}. 
+{inputs}
+Search Query: {query}. Rank the {n} passages above based on their relevance to the search query in descending order.
+"""
+
+
+class RerankLLM:
+    def __init__(
+        self,
+        model: EmbedLlamaForRankLM,
+        tokenizer: transformers.PreTrainedTokenizer,
+        encoder_tokenizer: transformers.PreTrainedTokenizer,
+    ) -> None:
+        self._model = model
+        self._tokenizer = tokenizer
+        self._encoder_tokenizer = encoder_tokenizer
+
+    def permutation_pipeline(
+        self,
+        result: dict,
+        rank_start: int,
+        rank_end: int,
+    ) -> list[dict]:
+        model_inputs = self.create_inputs(result, rank_start, rank_end)
+        permutation = self._model.rank(**model_inputs)
+        result = self.receive_permutation(
+            result, permutation, rank_start, rank_end)
+        return result
+
+    @torch.inference_mode()
+    def rerank(
+        self,
+        retrieved_result: dict,
+        window_size: Optional[int] = None,
+        step: Optional[int] = None,
+    ) -> list[dict]:
+        rerank_result = copy.deepcopy(retrieved_result)
+        rank_start, rank_end = 0, len(rerank_result["hits"])
+        if not window_size:
+            window_size = len(rerank_result["hits"])
+            step = window_size
+        else:
+            if not step:
+                step = window_size // 2
+        end_pos = rank_end
+        start_pos = rank_end - window_size
+        # end_pos > rank_start ensures that the list is non-empty while allowing last window to be smaller than window_size
+        # start_pos + step != rank_start prevents processing of redundant windows (e.g. 0-20, followed by 0-10)
+        while end_pos > rank_start and start_pos + step != rank_start:
+            start_pos = max(start_pos, rank_start)
+            rerank_result = self.permutation_pipeline(rerank_result, start_pos, end_pos)
+            end_pos = end_pos - step
+            start_pos = start_pos - step
+        return rerank_result
+
+    def receive_permutation(
+        self,
+        result: dict,
+        permutation: list[int],
+        rank_start: int,
+        rank_end: int
+    ):
+        cut_range = copy.deepcopy(result["hits"][rank_start:rank_end])
+        if min(permutation) != 0:
+            permutation = [x - min(permutation) for x in permutation]
+        for j, x in enumerate(permutation):
+            result["hits"][j + rank_start] = copy.deepcopy(cut_range[x])
+            if "rank" in result["hits"][j + rank_start]:
+                result["hits"][j + rank_start]["rank"] = cut_range[j]["rank"]
+            if "score" in result["hits"][j + rank_start]:
+                result["hits"][j + rank_start]["score"] = cut_range[j]["score"]
+        return result
+
+    def create_inputs(self, retrieval_results: dict, rank_start: int, rank_end: int) -> dict[str, torch.Tensor]:
+        retrieved_passages = retrieval_results["hits"][rank_start:rank_end]
+        model_inputs = {
+            "conversations": [
+                {
+                    "from": "human",
+                    "value": INSTRUCTION.format(
+                        n=len(retrieved_passages),
+                        query=retrieval_results["query"],
+                        inputs="\n".join(["[<PLACEHOLDER>]"] * len(retrieved_passages))
+                    )
+                },
+            ],
+            "extra_texts": [p["content"] for p in retrieved_passages]
+        }
+
+        # process data
+        conv = get_conversation_template("vicuna")
+        roles = {"human": conv.roles[0], "gpt": conv.roles[1]}
+        conversation = model_inputs["conversations"]
+        if roles[conversation[0]["from"]] != conv.roles[0]:
+            # Skip the first one if it is not from human
+            conversation = conversation[1:]
+        conv.messages = []
+        for j, sentence in enumerate(conversation):
+            role = roles[sentence["from"]]
+            assert role == conv.roles[j % 2]
+            conv.append_message(role, sentence["value"])
+        conv.append_message(conv.roles[1], None)
+        conversation = conv.get_prompt()
+
+        input_ids = self._tokenizer(
+            [conversation],
+            return_tensors="pt",
+            padding="longest",
+            max_length=self._tokenizer.model_max_length,
+            truncation=True,
+        ).input_ids
+
+        extra_text_input_ids = self._encoder_tokenizer(
+            model_inputs["extra_texts"],
+            return_tensors="pt",
+            padding="max_length",
+            max_length=200,
+            truncation=True,
+        ).input_ids.unsqueeze(0)
+
+        return {
+            "inputs": input_ids,
+            "extra_text_input_ids": extra_text_input_ids,
+            "extra_text_attention_mask": extra_text_input_ids.ne(self._encoder_tokenizer.pad_token_id)
+        }
